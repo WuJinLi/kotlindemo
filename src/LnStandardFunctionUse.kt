0bb731d48dd0254941eeb773845d60c938572037
wujinli <wujinli>
2021-07-06 11:16:00 +0800

    other:新增kotiln标准函数库调用

--- src/LnStandardFunctionUse.kt
+++ src/LnStandardFunctionUse.kt
@@ -1,7 +1,8 @@
 import java.io.File
+import java.nio.charset.Charset
 
 /**
- * kotiln标准函数库的使用 let 、apply、 run、 with
+ * kotiln标准函数库的使用 let 、apply、 run、 with、also、takeif、takeUnless
  */
 
 fun main() {
@@ -10,6 +11,11 @@ fun main() {
 //    standardOfNormalDealObj()
 //    standardOfApplyUse()
 //    standardOfRunUse()
+//    standardOfRunUse1()
+//    standardOfWithUse()
+//    standardOfAlsoUse()
+//    standardOfTakeifUse()
+    standardOfTakeUnlessUse()
 }
 
 /**
@@ -38,6 +44,21 @@ fun standardOfLetUse1() {
     println((1..10).shuffled().first().let {
         it * it
     })
+}
+
+/**
+ * also函数与let函数功能相似，和let一样，also将接受者作为值参传给lambda，但与一点also返回接受者对象，而let返回lambda结果；
+ * 因为这个差异，also尤其适合针对同一个原始对象，利用副作用做事，可以基于原始对象接受者执行额外的链式调用
+ */
+fun standardOfAlsoUse() {
+    var fileContents: List<String>
+    File("/Users/WuJinLi/Desktop/test.txt").also {
+        println(it.name)
+    }.also {
+        fileContents = it.readLines(Charsets.UTF_8)
+    }
+
+    println(fileContents)
 }
 
 /**
@@ -75,8 +96,65 @@ fun standardOfRunUse() {
     print(stud.toString())
 }
 
+/**
+ *run方法实现链式调用、::实现函数引用调用
+ */
+fun standardOfRunUse1() {
+    val str = "The people's Republic of China"
+    str.run(::isTooLong).run(::showMessage).run(::print)
+}
+
+/**
+ * with函数是run的辩题，他们的行为都是一样的，但是with的调用方式不通，调用with时需要值参作为其第一个参数传入
+ */
+fun standardOfWithUse() {
+    val str = "The people's Republic of China"
+    with(str) {
+        length >= 10
+    }.run(::print)
+}
+
+/**
+ * takeif需要判断lambda中提供的条件表达式，给出true或false，如果判断结果为true，则返回接受者对象，否则返回null，
+ * 如果需要某个条件是否满足，再决定是否可以赋值或者做其他执行任务，则takeif就可以去实现。
+ */
+fun standardOfTakeifUse() {
+    //使用takeif
+    File("/Users/WuJinLi/Desktop/test.txt").takeIf {
+        it.exists() && it.canRead()
+    }?.readText().run(::print)
+
+    //不使用takeif
+//    var file = File("/Users/WuJinLi/Desktop/test.txt")
+//    val fileContent: String? = if (file.exists() && file.canRead()) {
+//        file.readText()
+//    } else {
+//        null
+//    }
+//    print(fileContent)
+}
+
+/**
+ * takeunless:只有判断条件返回false时，才返回原始接收对象
+ */
+fun standardOfTakeUnlessUse() {
+    File("/Users/WuJinLi/Desktop/test.txt").takeUnless {
+        it.isHidden
+    }?.readText().run(::print)
+}
+
+
 private class Stud(var name: String, var age: Int) {
     override fun toString(): String {
         return "[name:$name,age:$age]"
+    }
+}
+
+fun isTooLong(str: String) = str.length > 10
+fun showMessage(isToolLong: Boolean): String {
+    return if (isToolLong) {
+        "str is too long ."
+    } else {
+        "please rename."
     }
 }
